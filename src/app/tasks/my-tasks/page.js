c6e228d0ecedcb50dbf5bd1f656f6bbfe6225cd7
Anen135 <whoyou1994j4920@gmail.com>
2025-08-08 13:34:32 +0500

    hotfix

--- src/app/tasks/my-tasks/page.js
+++ src/app/tasks/my-tasks/page.js
@@ -1,29 +1,13 @@
-'use client'
+// app/tasks/my-tasks/page.jsx
+import dynamic from 'next/dynamic'
 
-export const dynamic = "force-dynamic"
+const MyTasks = dynamic(() => import('@/components/MyTasks'), { ssr: false })
 
-import { useSession } from "next-auth/react"
-import useSWR from "swr"
-
-const fetcher = (url) => fetch(url).then(res => res.json())
-
-export default function MyTasks() {
-  const { data: session, status } = useSession()
-  const { data: tasks, error } = useSWR(session ? "/api/my-tasks" : null, fetcher)
-
-  if (status === "loading") return <p>Загрузка сессии...</p>
-  if (!session) return <p>Пожалуйста, войдите</p>
-  if (error) return <p>Ошибка при загрузке задач</p>
-  if (!tasks) return <p>Загрузка задач...</p>
-
+export default function Page() {
   return (
     <div>
-      <h2>Мои задачи ({tasks.length})</h2>
-      {tasks.map(task => (
-        <div key={task.id}>
-          <b>{task.content}</b> (в колонке: {task.column.title})
-        </div>
-      ))}
+      <h1>Страница: Мои задачи</h1>
+      <MyTasks />
     </div>
   )
 }
---
+++ src/components/tasks/MyTasks.js
@@ -0,0 +1,28 @@
+// components/MyTasks.jsx
+'use client'
+
+import { useSession } from "next-auth/react"
+import useSWR from "swr"
+
+const fetcher = (url) => fetch(url).then(res => res.json())
+
+export default function MyTasks() {
+  const { data: session, status } = useSession()
+  const { data: tasks, error } = useSWR(session ? "/api/my-tasks" : null, fetcher)
+
+  if (status === "loading") return <p>Загрузка сессии...</p>
+  if (!session) return <p>Пожалуйста, войдите</p>
+  if (error) return <p>Ошибка при загрузке задач</p>
+  if (!tasks) return <p>Загрузка задач...</p>
+
+  return (
+    <div>
+      <h2>Мои задачи ({tasks.length})</h2>
+      {tasks.map(task => (
+        <div key={task.id}>
+          <b>{task.content}</b> (в колонке: {task.column.title})
+        </div>
+      ))}
+    </div>
+  )
+}
