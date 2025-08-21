4f16ddb5c8b0e87f39a58ebd7754987bf53145ce
sult7717 <sult7717@gmail.com>
2025-08-21 09:46:40 +0500

    google login added

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,7 @@
 'use client';
 
 import ParticlesAnimation from '@/components/login/ParticlesAnimation';
-import GitHubLoginButton from '@/components/login/GitHubLoginButton';
+import {GitHubLoginButton, GoogleLoginButton} from '@/components/login/LoginButtons';
 
 export default function LoginPage() {
   return (
@@ -10,6 +10,7 @@ export default function LoginPage() {
       <div className="flex flex-col justify-center absolute p-8 bg-white/70 backdrop-blur-xl h-screen z-20 shadow-[2px_0_12px_rgba(0,0,0,0.05)] w-full max-w-[500px]">
         <h1 className="text-2xl md:text-[32px] font-bold mb-8 text-primary">Вход</h1>
         <GitHubLoginButton />
+        <GoogleLoginButton />
       </div>
       <ParticlesAnimation />
     </main>
